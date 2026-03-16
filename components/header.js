e9bab527068f8031084810285bd44ad112560cfb
dalma <dalmatithegoat@gmail.com>
2026-03-16 19:11:39 +0100

    9

--- components/header.js
+++ components/header.js
@@ -1,24 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Header({ title = "My First App" }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
+const Header = () => {
+    return(
+        <View style={StyleSheet.container}>
+            <Text style={styles.title}>Student App</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    paddingTop: 48,
-    paddingBottom: 12,
-    paddingHorizontal: 16,
-    backgroundColor: "#111827",
-  },
-  title: {
-    color: "#ffffff",
-    fontSize: 18,
-    fontWeight: "600",
-  },
+    container:{
+        backgroundColor: '#F3E3D0',
+        padding: 15
+    },
+
+    title:{
+        fontsize: 20,
+        color:"#81A6C6",
+        textAlign: "center"
+    }
 });
+
+export default Header;
