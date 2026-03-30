d9fb899ef85bd0b8d331efe36984ddac035d09ad
dalma <dalmatithegoat@gmail.com>
2026-03-30 18:43:37 +0200

    9

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native'
+import ProfileScreen from './screens/ProfileScreen'
 
 export default function App() {
-  return <HomeScreen />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Profile" component={ProfileScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,29 +1,44 @@
-import React from "react"
-import {View} from "react-native";
-import { Button}  from "react-native";
+import React from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import { Button } from "react-native";  
+
 
 import Header from "../components/header";
 import StudentCard from "../components/studentCard";
+import { students } from "../data/students";
+import { FlatList } from "react-native";
+import CustomButton from "../components/customButton";
 
-const HomeScreen = () =>{
-    return(
-        <View>
-            <CostumButton title="Click Me" onPress={() => console.log("CostumButton Pressed")}>
+const HomeScreen = ({navigation}) => {
+    return (
+        <View style={{ flex: 1 }}>
+  <Header />
 
-            </CostumButton>
-            
-            <Header/>
-            <StudentCard
-                name="John Doe"
-                course = "ReactNative"
-            />
-            <StudentCard
-            name = "Ana Smith"
-            course = "Mobile Development"
-            />
-            
-        </View>
-    );
+   <CustomButton
+    title="Go to Profile"
+    onPress={() => 
+      navigation.navigate('Profile',{
+        name: 'John Doe',
+        course: 'Computer Science'
+      })
+    }
+  />
+  
+  <FlatList
+    data={students}
+    keyExtractor={(item) => item.id.toString()}
+    renderItem={({ item }) => (
+      <StudentCard
+        name={
+item.name
+}
+        course={item.course}
+      />
+    )}
+  />
+ 
+</View>
+);
 };
 
-export default HomeScreen
+export default HomeScreen;
